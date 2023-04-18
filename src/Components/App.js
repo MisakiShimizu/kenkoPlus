a6642dbb99b1b35ee353e5d8473bced8a3d131d4
Misaki <tshimizumisaki@gmail.com>
2023-04-18 15:38:02 -0400

    Remove un-needed files and include logo

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import "../styles/sass/styles.css";
+import logo from "../styles/sass/assets/Plugin icon - 1 (1).png";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="Kenko plus logo." />
+      </header>
+    </div>
+  );
+}
+
+export default App;
