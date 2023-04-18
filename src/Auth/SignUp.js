721dc47dcec873fb0fd72c687b55903433f3014c
Misaki <tshimizumisaki@gmail.com>
2023-04-18 16:18:36 -0400

    Add layout for login

--- src/Auth/SignUp.js
+++ src/Auth/SignUp.js
@@ -1,2 +1,37 @@
+import React from "react";
 
+import { HiOutlineMail, HiOutlineUser, HiArrowRight } from "react-icons/hi";
+import { RiLockPasswordLine } from "react-icons/ri";
 
+export default function SignUp() {
+  return (
+    <div className="logContainer ">
+      <form action="">
+        <h2>Create Account</h2>
+        <label htmlFor="name">
+          <HiOutlineUser />
+          Your Name
+        </label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="email">
+          <HiOutlineMail />
+          Email
+        </label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">
+          <RiLockPasswordLine />
+          Password
+        </label>
+        <input type="password" name="password" id="password" />
+        <label htmlFor="password2">
+          <RiLockPasswordLine />
+          Re-enter Password
+        </label>
+        <input type="password" name="password2" id="password2" />
+        <button>
+          Sign Up <HiArrowRight />
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,27 @@
 import "../styles/sass/styles.css";
+import React, { useState } from "react";
 import logo from "../styles/sass/assets/Plugin icon - 1 (1).png";
+import SignUp from "../Auth/SignUp";
 function App() {
+  const [logIn, setLogIn] = useState(false);
+
+  const handleLogIn = () => {
+    setLogIn(true);
+  };
+
+  const handleSignUp = () => {
+    setLogIn(false);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="Kenko plus logo." />
       </header>
+      <section>
+        <button onClick={handleSignUp}>Sign Up</button>
+        <button onClick={handleLogIn}>Log In</button>
+        {logIn ? <p>login</p> : <SignUp />}
+      </section>
     </div>
   );
 }
